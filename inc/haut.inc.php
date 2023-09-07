2628e3354408488d9b9ed84625e0e735e50599c9
LSouryu <n.allary@hotmail.com>
2023-09-07 14:32:36 +0200

    Boo

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Site</title>
+    <link rel="stylesheet" href="<?php echo RACINE_SITE ?>inc/css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css%22%3E">
+    <script src="<?php echo RACINE_SITE ?>inc/js/main.css"></script>
 </head>
 <body>
     <header>
         <div class="conteneur">
-            <div>
+            <!-- <div>
                 <a href="" title="Mon Site">MonSite.com</a>
-            </div>
-            <nav>
+            </div> -->
+            <nav class="topnav" id="mytopnav">
                 <a href="<?php echo RACINE_SITE; ?>inscription.php"> Inscription</a>
                 <a href="<?php echo RACINE_SITE; ?>connexion.php"> Connexion</a>
                 <a href="<?php echo RACINE_SITE; ?>boutique.php"> Acces a la boutique</a>
--- inc/init.inc.php
+++ inc/init.inc.php
@@ -15,8 +15,8 @@ session_start();
 // print_r($_SERVER);
 define("RACINE_SITE", "http://" . $_SERVER['HTTP_HOST'] . "/");
 
-require('./inc/functions.inc.php');
-debug($mysqli, 0);
+// require('./inc/functions.inc.php');
+// debug($mysqli, 0);
 
 // define("RACINE_SITE", "http://localhost:4000/");
 
